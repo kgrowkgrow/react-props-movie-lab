18033d47bc070432f48196c06c1109571a4407af
kgrowkgrow <kevingrow11@gmail.com>
2020-12-13 11:28:00 -0800

    Done.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -14,30 +14,53 @@ import CardFront from './CardFront.js';
 import CardBack from './CardBack.js';
 
 
-const posterMap = {
-  'choux-maru-istanbul': cmi,
-  'choux-maru-part-1': cmp1,
-  'chromeboi': cb,
-  'escape-from-vim': efv,
-  'goldeneye': goldeneye,
-  'handsome-boy': hbmc,
-  'marus-spinoff': msts,
-  'terrance-king': tkr,
-  'the-trash-man': ttm,
-  'default': defaultPoster
-}
+// const posterMap = {
+//   'choux-maru-istanbul': cmi,
+//   'choux-maru-part-1': cmp1,
+//   'chromeboi': cb,
+//   'escape-from-vim': efv,
+//   'goldeneye': goldeneye,
+//   'handsome-boy': hbmc,
+//   'marus-spinoff': msts,
+//   'terrance-king': tkr,
+//   'the-trash-man': ttm,
+//   'default': defaultPoster
+// }
+
+
 
 export default class MovieCard extends Component {
-
+  
   render() {
+
+    const posterMap = {
+      'choux-maru-istanbul': cmi,
+      'choux-maru-part-1': cmp1,
+      'chromeboi': cb,
+      'escape-from-vim': efv,
+      'goldeneye': goldeneye,
+      'handsome-boy': hbmc,
+      'marus-spinoff': msts,
+      'terrance-king': tkr,
+      'the-trash-man': ttm,
+      'default': defaultPoster
+    }
+
+    const posterLink = posterMap[this.props.poster]
     return (
       <div className="movie-card">
-        {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        <CardFront poster={posterLink}/>
+        <CardBack title={this.props.title} IMDBRating={this.props.IMDBRating} genres={this.props.genres}/>
       </div>
     )
   }
+}
+
+MovieCard.defaultProps = {
+  title: 'Unknown',
+  IMDBRating: null,
+  genres: ['No Genre(s) Found'],
+  poster: 'default'
 }
 
 // Don't forget your default props!
